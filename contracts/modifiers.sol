3f3d63b0b305de883c3dd484a1929a2a00b3db2a
ISHaNT-CHaHaR <56401920+ISHaNT-CHaHaR@users.noreply.github.com>
2020-05-09 16:59:37 +0530

    Completed Modifier practice

---
+++ contracts/modifiers.sol
@@ -0,0 +1,20 @@
+pragma solidity >=0.4.25;
+
+
+contract Modifiers {
+    uint256 public minimumOffer = 100;
+
+    modifier minimumAmount() {
+        if (msg.value >= minimumOffer) {
+            _;
+        } else {
+            /** Throw an exception */
+            revert("Invalid!");
+        }
+    }
+
+    function bid() public payable minimumAmount returns (bool) {
+        // Code the adding a new bid
+        return true;
+    }
+}
