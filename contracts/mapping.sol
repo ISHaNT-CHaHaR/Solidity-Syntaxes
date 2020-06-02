9e3805519b63e3eea9d9af18cd0bc5af4f35c643
ISHaNT-CHaHaR <56401920+ISHaNT-CHaHaR@users.noreply.github.com>
2020-06-03 00:33:21 +0530

    added mappings

--- contracts/mapping.sol
+++ contracts/mapping.sol
@@ -1,4 +1,4 @@
-pragma solidity >=0.4.24;
+pragma solidity >=0.6.0;
 
 
 contract exampleMapping {
@@ -44,5 +44,42 @@ contract MappingsContract {
     // Remove the key value pair from the mapping
     function removeRelation(string memory name) public {
         delete (relations[name]);
+    }
+
+    mapping(address => uint256) balances;
+
+    mapping(address => mapping(address => bool)) approved; // exotic mapping
+
+    //array inside mapping
+    mapping(address => uint256[]) scores;
+
+    function map(address spender) external {
+        //1. adding element
+        balances[msg.sender] = 100;
+        //2. Read
+        balances[msg.sender];
+        //3. Update
+        balances[msg.sender] = 200;
+        //4.  Delete
+        delete balances[msg.sender];
+
+        /// default value
+        //balances[someAddressnotexist]; // this will have a value 0 because uint's default value is 0
+        // it depends according to mapped elements.
+
+        approved[msg.sender][spender] = true; // exotic mapping
+        approved[msg.sender][spender];
+        approved[msg.sender][spender] = false;
+        delete approved[msg.sender][spender];
+
+        /// array inside mapping
+        scores[msg.sender].push(1);
+        scores[msg.sender].push(2);
+
+        // read
+        scores[msg.sender][0];
+        scores[msg.sender][0] = 10;
+
+        delete scores[msg.sender][0];
     }
 }
